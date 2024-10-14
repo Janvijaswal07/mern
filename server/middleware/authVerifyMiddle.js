d02f3422f6a7bc3f60928d8ff25d77b9419ef8ff
janvi <laxmijaswal189@gmail.com>
2024-10-14 03:50:26 -0400

    add logic to verify the token

--- server/middleware/authVerifyMiddle.js
+++ server/middleware/authVerifyMiddle.js
@@ -1,11 +1,24 @@
 const jwt = require('jsonwebtoken');
+const user = require('../models/user-model')
 const authVerifyMiddle = async(req,res,next)=>{
 const token = req.header("Authorization")
 if(!token){
     res.status(401).json({msg:"token is not provider"})
 }
 const jwtToken = token.replace("Bearer","").trim();
-console.log(jwtToken);
-next();
+
+try {
+    const isVarify= jwt.verify(jwtToken,"secret");
+    
+    const userData = await user.findOne({email:isVarify.email}).select({password:0});
+    req.user=userData;
+    req.token=token;
+    req.id= userData._id;
+    console.log(userData);
+    next();
+} catch (error) {
+    res.status(401).json({msg:"token is invalid"})
 }
-module.exports= authVerifyMiddle;
+
+}
+module.exports= authVerifyMiddle; 
