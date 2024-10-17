bd89a075c879d39e9b03c56306ca61b946bce9f2
janvi <laxmijaswal189@gmail.com>
2024-10-17 02:57:26 -0400

    ass chalk

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const app = express();
 const cors = require('cors')
 const router = require('./router/auth-router');
 const connectDB = require('./utils/db');
+const service = require('./router/service-router')
 const contact = require('./router/contact-router');
 const { required } = require('./validator/auth-validator');
+const chalk = require("chalk")
 
  
 const corsOptions = {
@@ -18,11 +20,12 @@ const corsOptions = {
 app.use(express.json());
 app.use('/api/auth',router);
 app.use('/api/contactus',contact);
+app.use('/api/services',service)
 const port = process.env.PORT;
 
 connectDB().then(()=>{
     app.listen(port,()=>{
-        console.log(`server is running on ${port}`);
+        console.log(chalk.cyanBright`server is running on ${port}`);
     });
 });
  
