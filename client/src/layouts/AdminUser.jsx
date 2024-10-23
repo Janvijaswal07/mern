b448e718d95f184a8cb243c7134d62a335aa27a7
janvi <laxmijaswal189@gmail.com>
2024-10-23 03:33:02 -0400

    add adminUser data

--- client/src/layouts/AdminUser.jsx
+++ client/src/layouts/AdminUser.jsx
@@ -1,18 +1,25 @@
 
+import { useEffect } from 'react';
 
 const AdminUser = () => {
+   const token= localStorage.getItem("token")
     const getUserAllData = async ()=>{
         try {
             const response =await fetch("http://localhost:3000/api/admin/user",{
-                method="GET",
-                headers={
-                    Au
-                }
+                method:"GET",
+                headers:{
+                    Authorization:`Bearer ${token}`
+                },
             })
+            const data = await response.json();
+            console.log("hlo");
         } catch (error) {
-            
+            console.log("error from adminUser",error);
         }
     }
+    useEffect(()=>{
+        getUserAllData();
+},[])
   return (
     <div>AdminUser</div>
   )
