7f3544c224e71798ba699473a64224cf0250619a
janvi <laxmijaswal189@gmail.com>
2024-10-23 00:53:13 -0400

    add fetch funtion

--- client/src/layouts/AdminUser.jsx
+++ client/src/layouts/AdminUser.jsx
@@ -1,6 +1,18 @@
 
 
 const AdminUser = () => {
+    const getUserAllData = async ()=>{
+        try {
+            const response =await fetch("http://localhost:3000/api/admin/user",{
+                method="GET",
+                headers={
+                    Au
+                }
+            })
+        } catch (error) {
+            
+        }
+    }
   return (
     <div>AdminUser</div>
   )
