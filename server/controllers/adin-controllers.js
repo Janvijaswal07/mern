eeca23aa42414ca657f40158beaafc942989e4f2
janvi <laxmijaswal189@gmail.com>
2024-10-23 03:34:56 -0400

    solve the http header issue

--- server/controllers/adin-controllers.js
+++ server/controllers/adin-controllers.js
@@ -1,21 +1,22 @@
 const users= require('../models/user-model');
 const contact = require ('../models/contact-model')
-const getAllUers= async (req,res)=>{
+const getAllUers = async (req, res) => {
     try {
-        const userData= await users.find();
-        if(!userData||userData===0){
-            res.status(400).json({msg:"user is not available"})
-        }
-        res.status(200).json({msg:userData});
+      const userData = await users.find();
+      if (!userData || userData.length === 0) {
+        return res.json({ msg: "User is not available" }); 
+      }
+     return res.json({ msg: userData });
     } catch (error) {
-        res.status(400).json({msg:"error from user route"})
+      res.json({ msg: "Error from user route" });
     }
-}
+  };
+  
 const getContactData = async(req,res)=>{
     try {
         const contactData = await contact.find();
         if(!contactData||contactData===0){
-            res.status(400).json({msg:"contactData is not available"})  
+           return res.status(400).json({msg:"contactData is not available"})  
         }
         res.status(200).json({msg:contactData});
     } catch (error) {
